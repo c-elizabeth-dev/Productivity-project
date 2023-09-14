3cfdf1ed086994e3f04ce581f13cd7af39670f27
c-elizabeth-dev <c.elizabeth.keane@gmail.com>
2023-09-14 10:56:07 +0100

    separate js files

---
+++ todo-script.js
@@ -0,0 +1,81 @@
+//* To do --- start---
+
+
+
+let checkElements = document.getElementsByClassName('circle-check-todo');
+
+
+for (let i=0; i < checkElements.length; i++) {
+    checkElements[i].addEventListener('click', function () {
+        this.classList.toggle('circle-done')
+    })
+}
+
+let newTask = document.querySelector('.appended-area');
+let textbox = document.querySelector('.new-task-input');
+let addButton = document.querySelector('.submit');
+
+
+// document.addEventListener('keydown', function (event) {
+//   let patternForText = /[0-9]/g;
+//   let patternForOperatorsText = /[+\-*\/]/g
+//   if (event.key.match(patternForText)) {
+//     event.preventDefault();
+//     newTask.appendNumber(event.key)
+//     newTask.updateDisplay()
+//   }
+// });
+
+
+textbox.addEventListener('keydown', function (event) {
+    if (event.key === 'Backspace') {
+      event.preventDefault();
+      event.stopPropagation();                               //* Prevent the default backspace behavior which navigates back in the browser
+      textbox.value = textbox.value.slice(0, -1);           //* Remove the last character from the input value
+    } else if (event.key === 'Enter') {
+      event.preventDefault();
+      event.stopPropagation(); 
+      addButton.click();
+    }
+  });
+
+
+addButton.addEventListener('click', function takeData(){    //* when button is clicked then execute this function
+let taskText = textbox.value;
+if (taskText.trim() !== '') {                               //* if not equal to empty (whitespace is trimmed) then execute following
+
+let taskContainer = document.createElement('div');
+taskContainer.classList.add('new-created-task');
+
+    let newText=document.createElement('div');
+    newText.classList.add('new-text');
+    newText.textContent=taskText;
+
+    let newCheck = document.createElement('div');
+    newCheck.classList.add('circle-check-todo');
+
+    let deleteIcon = document.createElement('ion-icon');
+    deleteIcon.classList.add ('delete-icon');
+    deleteIcon.setAttribute('name', 'trash');
+    deleteIcon.addEventListener('click', function(){
+        newTask.removeChild(taskContainer);
+    });
+
+
+    taskContainer.appendChild(newCheck);
+    taskContainer.appendChild(newText);
+    taskContainer.appendChild(deleteIcon);
+
+newTask.appendChild(taskContainer);
+
+// localStorage.setItem(newTask);
+
+textbox.value= '';
+}
+});
+
+document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('circle-check-todo')) {
+        event.target.classList.toggle('circle-done');
+    }
+});
